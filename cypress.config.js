4e5b6ae68e635f60113f998e97d378095471722d
Renan Ramos <renanlramos1996@gmail.com>
2022-06-18 18:31:43 -0300

    adding info cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "jhsr72",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
